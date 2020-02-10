d8233c4ab64cbf6be83852cf43eb8799fae0e0e7
Manuelpv17 <manuelpv17@outlook.com>
2020-02-10 22:49:46 +0000

    Add Task 4. isalpha --version1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+int _isalpha(int c)
+{
+	int i;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; i < 52; i++)
+	{
+		if (alp[i] == c)
+		{
+		return(1);
+		}
+	}
+	return (0);
+}
