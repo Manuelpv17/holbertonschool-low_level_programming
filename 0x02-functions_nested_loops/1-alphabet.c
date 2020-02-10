1abff7b63cf603337cc882d98d8a86157d59c3e6
Manuelpv17 <manuelpv17@outlook.com>
2020-02-10 22:18:19 +0000

    Add Task 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game --version1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+void print_alphabet(void)
+{	
+	int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alp[i]);
+	}
+	_putchar('\n');
+	return;
+}
