4f11d31277736bfe42b45537f69b343ef8a50d55
Manuelpv17 <manuelpv17@outlook.com>
2020-02-11 16:49:08 +0000

    Add Task 11. Holberton School, 98 Battery Street, the OG --version3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
 * print_to_98 - print
 * @n: int
@@ -6,41 +7,24 @@
 void print_to_98(int n)
 {
 	int i;
-	int n1;
 
 	for (i = n;;)
 	{
-		if (i >= 0 && i <= 9)
+		if (i >= 0)
 		{
-			_putchar(i + '0');
-		} else if (i >=  10 && i <= 99)
-		{	_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-		} else if (i >= -9 && i <= -1)
+			printf("%d", i);
+		} else
 		{
-			n1 = -i;
-			_putchar('-');
-			_putchar(n1 + '0');
-		} else if (i >= -99 && i <= -10)
-		{
-			n1 = -i;
-			_putchar('-');
-			_putchar(n1 / 10 + '0');
-			_putchar(n1 % 10 + '0');
-		} else if (i >= 100 && i <= 999)
-		{
-			_putchar(i / 100 + '0');
-			_putchar((i % 100) / 10 + '0');
-			_putchar((i % 100) % 10 + '0');
-		}
+			printf("%d", i);
+		}		
 		if (i == 98)
 			break;
-		_putchar(',');
-		_putchar(' ');
+		printf(",");
+		printf(" ");
 		if (i > 97)
 			i--;
 		else if (i < 99)
 			i++;
 	}
-	_putchar('\n');
+	printf("\n");
 }
