c535ff65563f1b8c8eefa563bf5311b3b2945c3d
Manuelpv17 <manuelpv17@outlook.com>
2020-02-11 10:24:53 +0000

    Add Task 7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important  --version3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,17 @@
 #include "holberton.h"
 
+/**
+  *print_last_digit - last digit
+  *@n: First operand
+  *Return: last digit
+  */
 int print_last_digit(int n)
 {
 	if (n < 0)
 	{
 		n = -n;
 	}
-	if (n < 10 && n > 0)
+	if (n < 10 && n > -1)
 	{
 		_putchar(n + '0');
 		return (n);
