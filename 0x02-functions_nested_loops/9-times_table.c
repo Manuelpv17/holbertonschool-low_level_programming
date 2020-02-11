8dfe8f9cf9d97050aeec9bdf3607e439f78038e3
Manuelpv17 <manuelpv17@outlook.com>
2020-02-11 11:06:19 +0000

    Add Task 9. Learn your times table --version2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,34 @@
 #include "holberton.h"
 
+/**
+  *times_table - table
+  *@void: void
+  *
+  */
+
 void times_table(void)
 {
 	int i;
 	int j;
+
 	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (i*j < 10)
+			if (j == 0)
+			{
+				_putchar('0');
+			} else if (i * j < 10)
 			{
 				_putchar(' ');
-				_putchar(i*j + '0');
-			}
-			else
+				_putchar(i * j + '0');
+			} else
 			{
-				_putchar((i*j)/10 + '0');
-				_putchar((i*j)%10 + '0');
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
 			}
-			
-			if ( j == 9 )
+
+			if (j == 9)
 				break;
 
 			_putchar(',');
@@ -27,6 +36,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-
-	return;
 }
