0e706a1c9ffce28fb465a279a0cc82de7c8d99bd
Manuelpv17 <manuelpv17@outlook.com>
2020-02-11 18:07:49 +0000

    Add Task 13. Nature made the natural numbers; All else is the work of women --version2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main - entry point
+ * Return: 0
  *
  */
 int main(void)
 {
-	int s = 0; 
-	int t = 10; 
+	int s = 0;
+	int t = 1024;
 	int i;
 
 	for (i = 0; i < t; i++)
-		if ( i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			s = s + i;
 		}
