7af0c72995e39e7babff7cf1d84bd5581e145d1b
Manuelpv17 <manuelpv17@outlook.com>
2020-02-13 16:12:45 +0000

    Add Task 7. I feel like I am diagonally parked in a parallel universe --version1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Function
+ * @n: Int
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar(32);
+			if (i == j)
+			{
+				_putchar(92);
+			}
+		}
+		_putchar(10);
+	}
+	_putchar(10);
+}
