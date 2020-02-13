7fc4c8b97ba8c5112b62400e414e1ce4d0e6a0da
Manuelpv17 <manuelpv17@outlook.com>
2020-02-13 20:10:33 +0000

    Add Task 11. The problem of distinguishing prime numbers --version1

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	int j;
+	long int n = 612852475143;
+	long int f = 0;
+
+	for (j = 0; j <= n; j++)
+	{
+		for (i = 2; i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				n = n / i;
+				if (i >= f)
+					f = i;
+				break;
+			}
+		}
+	}
+	printf("%ld", f);
+	printf("\n");
+	return (0);
+}
+
