d43d67b46001832e60126d40941ab160afeb3af8
Manuelpv17 <manuelpv17@outlook.com>
2020-02-13 15:14:08 +0000

    Add Task 0.isdigit --version1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Function
+ * @c: Int
+ * Return: isupper
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _isupper(int c);
+int _putchar(char);
+int _isdigit(int c);
+
+#endif
