b737eb2cee94931f2de7c172471ff2175f661b85
Manuelpv17 <manuelpv17@outlook.com>
2020-02-14 00:36:34 +0000

    Add Task 12. Numbers have life version2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,16 @@
  */
 void print_number(int n)
 {
+	unsigned n1;
+
 	if (n < 0)
 	{
 		n = -1 * n;
 		_putchar('-');
 	}
+	n1 = n;
 
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar(n1 % 10 + '0');
 }
