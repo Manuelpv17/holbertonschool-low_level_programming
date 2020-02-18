6af2a5ef7565d2eb07db99b96b211a9b80d36408
Manuelpv17 <manuelpv17@outlook.com>
2020-02-18 15:54:58 +0000

    Add Task 6. Half the lies they tell about me aren't true  --version2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,14 @@
  */
 void puts2(char *str)
 {
-	int i;
-	int j;
+	int i = 1;
+	int j = 0;
 
 	for (i = 1; str[i] != '\0'; i++)
 	{
 	}
 
-	for (j = 0; j < i - 1; j++)
+	for (j = 0; j < i; j++)
 	{
 		if (j % 2 == 0)
 			_putchar(str[j]);
