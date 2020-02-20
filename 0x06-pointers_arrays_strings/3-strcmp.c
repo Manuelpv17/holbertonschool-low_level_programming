a61bf8f87bd6911aef144dac4a907b7e21cfbf05
Manuelpv17 <manuelpv17@outlook.com>
2020-02-20 13:46:45 -0500

    Add Task 2. strncpy --version4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * _strncmp - Compares
- * @s1: pointer first string 
+ * _strcmp - Compares
+ * @s1: pointer first string
  * @s2: pointer second string
- * Return: 
+ * Return: compare
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	
+
 	for (i = 0; (s1[i] == s2[i]) && s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 	}
