efb5e4f2a73e78f0e2b7fccc5a1f6774e714f55e
Manuelpv17 <manuelpv17@outlook.com>
2020-02-20 13:42:11 -0500

    Add Task 2. strncpy --version3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-	int n = 0;
-
-	for (j = 0; s1[j] != '\0'; j++)
-	{
-	}
-
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-	}
+	int i;
 	
-	if (j > i)
-		n = 15;
-	else if (j < i)
-		n = -15;
-	else
-		n = 0;
+	for (i = 0; (s1[i] == s2[i]) && s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+	}
 
-	return (n);
+	return (s1[i] - s2[i]);
 
 }
