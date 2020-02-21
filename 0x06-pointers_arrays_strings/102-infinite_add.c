e403cd1e037423b7ec62474bc17f42392c62a554
Manuelpv17 <manuelpv17@outlook.com>
2020-02-20 22:55:48 -0500

    Add Task  11. It is the addition of strangeness --version4

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * infinite_add - adds two numbers.
+ *  infinite_add - adds two numbers.
  * @n1: number 1
  * @n2: number 2
  * @r: Array return
@@ -9,7 +9,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, i1, j, k, a, l, n;
+	int i, j, k, a, l, n;
 	int c = 0;
 
 	for (i = 0; n1[i] != '\0'; i++)
@@ -18,11 +18,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (j = 0; n2[j] != '\0'; j++)
 	{
 	}
-	if (i < j)
-		i1 = j;
-	else
-		i1 = i;
-	for (k = 0; k < i1; k++)
+	for (k = 0; k < i || k < j; k++)
 	{
 		n = 0;
 		if (i > k)
@@ -43,14 +39,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[k] = '1';
 		k++;
 	}
-
 	for (l = 0; l < k / 2; l++)
 	{
 		a = r[l];
 		r[l] = r[k - l - 1];
 		r[k - l - 1] = a;
 	}
-
 	r[k] = '\0';
 	if (k >= size_r)
 		return (0);
