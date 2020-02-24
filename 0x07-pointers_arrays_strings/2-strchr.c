45264a15614a07e70cdd7c44b5a359d32ad9c1f1
Manuelpv17 <manuelpv17@outlook.com>
2020-02-24 13:57:48 -0500

    Add Task 2. strchr -v6

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,6 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 	char *p;
 
-	if (c == '\0')
-	{
-		return('\0');
-	}
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -24,6 +20,7 @@ char *_strchr(char *s, char c)
 			return (p);
 		}
 	}
-	p = &s[i];
-	return (p);
+	if (c == '\0')
+		p = &s[i];
+	return ('\0');
 }
