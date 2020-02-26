1787a30b676e54572e1c0a89b26f0859959bbb27
Manuelpv17 <manuelpv17@outlook.com>
2020-02-26 12:34:39 -0500

    Add Task 0. She locked away a secret -v2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	if (s[i] != '\0')
-		_puts_recursion(&s[i + 1]);
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(&s[1]);
+	}
 	else
 		_putchar('\n');
 
