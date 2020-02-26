2f135c575a99991a573e68afa40ac32e884e03e7
Manuelpv17 <manuelpv17@outlook.com>
2020-02-26 13:31:02 -0500

    Add Task 5. Your subconscious is looking for the dreamer -v1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+int aux(int, int);
+
+/**
+ * _sqrt_recursion - he natural square root of a number.
+ * @n: int n
+ * Return: Natural square root n
+ */
+int _sqrt_recursion(int n)
+{
+	int s;
+
+	s = aux(n, n);
+	return (s);
+}
+
+/** 
+ * aux - a
+ * @n: a
+ * @cont: a
+ * Return: a 
+ */
+int aux(int n, int cont)
+{
+	if (n == cont * cont)
+		return (cont);
+	else if (cont <= 0)
+		return (-1);
+	else if (cont > 0)
+	 	cont = aux(n, cont - 1);
+
+	return (cont);
+}
+
