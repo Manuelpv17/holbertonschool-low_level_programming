362b784c0ab2cbb4b55ee391d6f8994e1d6a77e8
Manuelpv17 <manuelpv17@outlook.com>
2020-03-02 11:57:52 -0500

    Add Task 8. Inception -v7

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,7 +1,5 @@
 #include "holberton.h"
 
-int _strlen_recursion(char *s);
-int aux(int l1, int l2, char *s1, char *s2);
 /**
  * wildcmp - compares two strings
  * @s1: string compare 1
@@ -10,30 +8,25 @@ int aux(int l1, int l2, char *s1, char *s2);
  */
 int wildcmp(char *s1, char *s2)
 {
-	int c;
+	int p = 0;
 
-	if (s1[0] == '\0' && s1[0] == s2[0])
+	if (s1[0] == s2[0] && s1[0] == '\0' && s2[0] == '\0')
 		return (1);
-
+	
 	else if (s1[0] == s2[0])
-		c = wildcmp(&s1[1], &s2[1]);
+		p = wildcmp(&s1[1], &s2[1]);
 
-	else if (s2[0] == '*' && s2[1] == '*')
-		c = wildcmp(&s1[0], &s2[1]);
+	else if (s2[0] == '*')
+	{
+		if (s1[1] == s2[1])
+			p = wildcmp(&s1[1], &s2[1]);
 
-	else if (s2[0] == '*' && s1[0] != s2[1])
-		c = wildcmp(&s1[1], &s2[0]);
+		if (p != 1)
+			p = wildcmp(&s1[1], &s2[0]);
 
-	else if (s2[0] == '*' && s1[10] == s2[1])
-		c = wildcmp(&s1[10], &s2[1]);
+		if (p != 1)
+			p = wildcmp(&s1[0], &s2[1]);
+	}
 
-	else if (s2[0] == '*' && s1[5] == s2[1])
-		c = wildcmp(&s1[5], &s2[1]);
-
-	else if (s2[0] == '*' && s1[0] == s2[1])
-		c = wildcmp(&s1[0], &s2[1]);
-	else
-		return (0);
-
-	return (c);
+	return (p);
 }
