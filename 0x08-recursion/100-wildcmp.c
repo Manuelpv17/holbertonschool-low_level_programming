269d2be80ad2c197b91e2fdabbc79357846bab61
Manuelpv17 <manuelpv17@outlook.com>
2020-03-02 15:22:02 -0500

    Add Task 8. Inception -v8

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -10,22 +10,24 @@ int wildcmp(char *s1, char *s2)
 {
 	int p = 0;
 
-	if (s1[0] == s2[0] && s1[0] == '\0' && s2[0] == '\0')
+	if (s1[0] == '\0' && s2[0] == '\0')
 		return (1);
-	
+
 	else if (s1[0] == s2[0])
 		p = wildcmp(&s1[1], &s2[1]);
 
 	else if (s2[0] == '*')
 	{
-		if (s1[1] == s2[1])
+		if (s2[1] == '*')
+			p = wildcmp(&s1[0], &s2[1]);
+		else if (s1[1] != '\0')
+			p = wildcmp(&s1[1], &s2[0]);
+		else
 			p = wildcmp(&s1[1], &s2[1]);
 
-		if (p != 1)
-			p = wildcmp(&s1[1], &s2[0]);
-
-		if (p != 1)
+		if (p != 1 && s2[1] != '*')
 			p = wildcmp(&s1[0], &s2[1]);
+
 	}
 
 	return (p);
