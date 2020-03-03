bcdd0ecaa3bcb685c4b985685268bea742cf1400
Manuelpv17 <manuelpv17@outlook.com>
2020-03-03 10:48:43 -0500

    Add Task 1. The woman -v1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy string in new space
+ * @str: String to copy
+ * Return: pointer to a newly allocated space
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *p = NULL;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	p = malloc(sizeof(char) * i);
+
+	if (p == NULL)
+	{
+		return (p);
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		p[j] = str[j];
+	}
+
+	return (p);
+}
