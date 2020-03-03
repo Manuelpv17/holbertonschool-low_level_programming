daf4b86b39da3aab4be4c3ff3ab5184a7695ee8e
Manuelpv17 <manuelpv17@outlook.com>
2020-03-03 15:32:06 -0500

    Add Task 3. If you even dream -v3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,11 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
 		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+			return (NULL);
+	}
 
 	for (i = 0; i < width; i++)
 	{
