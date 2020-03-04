ca8a4804bd441871026dc11453c52b1deeac94cc
Manuelpv17 <manuelpv17@outlook.com>
2020-03-04 17:07:10 -0500

    Add Task 6. I will show you how great I am -v2

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,6 +1,7 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int count_words(char *);
 
 /**
  * strtow - splits a string into words
@@ -8,44 +9,67 @@
  * Return: pointer to pointers of strings
  */
 char **strtow(char *str)
-{	char **p = NULL;
-	int i, k, c, a, c2, c3;
+{
+	char **p = NULL;
+	int i, j, nWords, c2, c3;
 
-	c = c2 = c3 = a = 0;
+	c2 = c3 = 0;
+
 	if (str == NULL)
 		return (NULL);
+
+	nWords = count_words(str);
+
+	p = malloc(sizeof(char *) * nWords + 1);
+
+	if (p == NULL || nWords == 0)
+		return (NULL);
+
+	for (i = 0; c3 < nWords; i++)
+	{
+		if (str[i] != ' ' && str[i] != '\0')
+		{
+			c2++;
+		}
+		else if (c2 > 0 && (str[i] == ' ' || str[i] == '\0'))
+		{
+			p[c3] = malloc(sizeof(char) * c2 + 1);
+
+				if (p[c3] == NULL)
+				{
+					for (j = 0; j < c3; j++)
+						free(p[j]);
+					free(p);
+					return (NULL);
+				}
+
+			for (j = 0; j < c2; j++)
+			{
+				p[c3][j] = str[i - c2 + j];
+			}
+			p[c3][j] = '\0';
+			c3++;
+			c2 = 0;
+		}
+	}
+	p[c3 + 1] = NULL;
+	return (p);
+}
+
+/**
+ * count_words - Amount of words in string
+ * @str: String to search
+ * Return: count of words
+ */
+int count_words(char *str)
+{
+	int i, c;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			c++;
 	}
-	p = malloc(sizeof(char *) * c + 1);
-	if (p == NULL || c == 0)
-		return (NULL);
-	for (i = 0; str[i] != '\0' || c3 < c; i++)
-	{
-		if (str[i] != ' ' && str[i] != '\0')
-		{
-			c2++;
-			a = 1;
-		}
-		else if (a == 1 && (str[i] == ' ' || str[i] == '\0'))
-		{
-			p[c3] = malloc(sizeof(char) * c2 + 1);
-				if (p[c3] == NULL)
-				{
-					for (k = 0; k < c3; k++)
-						free(p[k]);
-					free(p);
-					return (NULL);
-				}
-
-			for (k = 0; k < c2; k++)
-			{
-				p[c3][k] = str[i - c2 + k];
-			} p[c3][k] = '\0';
-			c3++;
-			a = c2 = 0;
-		}
-	}	return (p);
+	return (c);
 }
+
