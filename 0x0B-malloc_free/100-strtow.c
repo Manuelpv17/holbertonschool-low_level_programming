fccfcded5d9872653d4ea5aa020f2246221a1a48
Manuelpv17 <manuelpv17@outlook.com>
2020-03-03 23:48:26 -0500

    Add Task 6. I will show you how great I am -v4

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -12,25 +12,24 @@ char **strtow(char *str)
 	int i, k, c, a, c2, c3;
 
 	c = c2 = c3 = a = 0;
-	if (str == NULL || str[0] == 0)
+	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i - 1] == ' ' && str[i] != ' ' && str[i] != '\0')
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			c++;
 	}
 	p = malloc(sizeof(char *) * c + 1);
-	if (p == NULL)
+	if (p == NULL || c == 0)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0' || c3 < c; i++)
 	{
 		if (str[i] != ' ' && str[i] != '\0')
 		{
 			c2++;
 			a = 1;
 		}
-		else if (a == 1 && str[i] == ' ')
+		else if (a == 1 && (str[i] == ' ' || str[i] == '\0'))
 		{
 			p[c3] = malloc(sizeof(char) * c2 + 1);
 				if (p[c3] == NULL)
