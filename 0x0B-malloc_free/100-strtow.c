792b6b49988b8385aef7524af171340ed6b6d1e7
Manuelpv17 <manuelpv17@outlook.com>
2020-03-03 19:04:05 -0500

    Add Task 6. I will show you how great I am -v0

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @grid: 2 dimensional array
+ * Return: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	char **p;
+	int i;
+	
+	for (i = 0; str[i] != 1; i++)
+	{
+	}
+
+	return (p);
+}
