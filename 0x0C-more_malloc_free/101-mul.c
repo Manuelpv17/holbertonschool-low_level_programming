d56626e527d559f7be552c59b7a40123735cb6a7
Manuelpv17 <manuelpv17@outlook.com>
2020-03-06 00:13:35 -0500

    Add Task 5. We must accept finite disappointment, but never lose infinite hope -v0.1

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,9 @@
+#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _puts(char *str);
 int _strlen(char *s);
@@ -14,11 +16,10 @@ int _strlen(char *s);
 int main(int argc, char *argv[])
 {
 	char **p;
-	int i;
-	int n = 1;
-
-	int len1 = _srtlen(argv[1]);
-	int len2 = _srtlen(argv[2]);
+	int i, j;
+	int len1;
+	int len2;
+	int  n;
 
 	if (argc != 3)
 	{
@@ -26,29 +27,56 @@ int main(int argc, char *argv[])
 		exit (98);
 	}
 
-	p = malloc(sizeof(char *) * _strlen(argv[1]));
+	len1 = _strlen(argv[1]);
+	len2 = _strlen(argv[2]);
+
+	p = malloc(sizeof(char *) * 3);
 	
 	if (p == NULL)
+		return ('\0');
+
+	for (i = 0; i < len1; i++)
 	{
-		return (NULL);
+		p[i] = calloc(len2 + 1, sizeof(char));
+
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(p[j]);
+			free(p);
+			return ('\0');
+		}
 	}
 
-	for ()
+	for (i = 0; i < len1; i++)
 	{
+		for (j = 0; j < len2; j++)
+		{
+			if (i == 1 && j == 0)
+				p[i][j] = '0';
+				
+			else
+			{
+				n = (argv[1][len1 -1 -j] - '0') * (argv[2][len2 - i - 1] - '0');
+				p[i][j] = n + '0';
+			}
+		}
 	}
 
-	for (i = 0; i < _strlen)
+	for (i = 0; i < len1; i++)
 	{
-		p[i] = malloc(sizeof(char) * );
+		for (j = 0; j < len2; j++)
+		{
+			_putchar(p[i][j]);
+		}
+		_putchar('\n');
 	}
 
-	for (i = 1; i < argc; i++)
-	{
-		 = n * ( argv[i][0] - '0');
-	}
 	
+	for (j = 0; j < len1; j++)
+		free(p[j]);
 	free(p);
-
+	
 	return (0);
 }
 
@@ -126,5 +154,59 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	free(ptr);
 
 	return (p);
+}
+
+
+
+/**
+ *  infinite_add - adds two numbers.
+ * @n1: number 1
+ * @n2: number 2
+ * @r: Array return
+ * @size_r: max size
+ * Return: r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, a, l, n;
+	int c = 0;
+
+	for (i = 0; n1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; n2[j] != '\0'; j++)
+	{
+	}
+	for (k = 0; k < i || k < j; k++)
+	{
+		n = 0;
+		if (i > k)
+			n = n + n1[i - k - 1] - '0';
+		if (j > k)
+			n = n + n2[j - k - 1] - '0';
+		n = n + c;
+		c = 0;
+		if (n > 9)
+		{
+			r[k] = ((n - 10) + '0');
+			c = 1;
+		} else
+			r[k] = n + '0';
+	}
+	if (c == 1)
+	{
+		r[k] = '1';
+		k++;
+	}
+	for (l = 0; l < k / 2; l++)
+	{
+		a = r[l];
+		r[l] = r[k - l - 1];
+		r[k - l - 1] = a;
+	}
+	r[k] = '\0';
+	if (k >= size_r)
+		return (0);
+	return (r);
 }
 
