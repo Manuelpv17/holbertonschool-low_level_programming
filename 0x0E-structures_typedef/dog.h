0da6a46636ffa3d09eb9f2cda9f8fe2954a312bf
Manuelpv17 <manuelpv17@outlook.com>
2020-03-09 13:55:42 -0500

    Add Task 0. Django  -v1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * dog - My dog structure
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: My dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
