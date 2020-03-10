655e8294454018108f9d0246862a8011eb2daa0c
Manuelpv17 <manuelpv17@outlook.com>
2020-03-09 19:47:12 -0500

    Add Test of task5. Worst abuse of the C preprocessor (IOCCC winner, 1986)

--- 0x0D-preprocessor/hague.c
+++ 0x0D-preprocessor/hague.c
@@ -1,28 +1,27 @@
-#define DIT (
-#define DAH )
-#define __DAH   ++
-#define DITDAH  *
-#define DAHDIT  for
-#define DIT_DAH malloc
-#define DAH_DIT gets
-#define _DAHDIT char
-_DAHDIT _DAH_[]="ETIANMSURWDKGOHVFaLaPJBXCYZQb54a3d2f16g7c8a90l?e'b.s;i,d:"
-;main           DIT         DAH{_DAHDIT
-DITDAH          _DIT,DITDAH     DAH_,DITDAH DIT_,
-DITDAH          _DIT_,DITDAH        DIT_DAH DIT
-DAH,DITDAH      DAH_DIT DIT     DAH;DAHDIT
-DIT _DIT=DIT_DAH    DIT 81          DAH,DIT_=_DIT
-__DAH;_DIT==DAH_DIT DIT _DIT        DAH;__DIT
-DIT'\n'DAH DAH      DAHDIT DIT      DAH_=_DIT;DITDAH
-DAH_;__DIT      DIT         DITDAH
-_DIT_?_DAH DIT      DITDAH          DIT_ DAH:'?'DAH,__DIT
-DIT' 'DAH,DAH_ __DAH    DAH DAHDIT      DIT
-DITDAH          DIT_=2,_DIT_=_DAH_; DITDAH _DIT_&&DIT
-DITDAH _DIT_!=DIT   DITDAH DAH_>='a'?   DITDAH
-DAH_&223:DITDAH     DAH_ DAH DAH;       DIT
-DITDAH          DIT_ DAH __DAH,_DIT_    __DAH DAH
-DITDAH DIT_+=       DIT DITDAH _DIT_>='a'?  DITDAH _DIT_-'a':0
-DAH;}_DAH DIT DIT_  DAH{            __DIT DIT
-DIT_>3?_DAH     DIT          DIT_>>1 DAH:'\0'DAH;return
-DIT_&1?'-':'.';}__DIT DIT           DIT_ DAH _DAHDIT
-DIT_;{DIT void DAH write DIT            1,&DIT_,1 DAH;}
+#define((
+#define))
+#define++ ++
+#define **
+#define for  for
+#define malloc malloc
+#define gets gets
+#define char char
+char alphabet[] = "ETIANMSURWDKGOHVFaLaPJBXCYZQb54a3d2f16g7c8a90l?e'b.s;i,d:";
+main() {
+  char *p1, *p2, *p3, *p4, *malloc(), *gets();
+  for (p1 = malloc(81), p3 = p1++; p1 == gets(p1); print('\n'))
+    for (p2 = p1; *p2;
+         print(*p4 ? translate(*p3) : '?'), print(' '), p2++)
+      for (*p3 = 2, p4 = alphabet;
+           *p4 && (*p4 != (*p2 >= 'a' ? *p2 & 223 : *p2));
+           (*p3)++, p4++)
+        *p3 += (*p4 >= 'a' ? *p4 - 'a' : 0);
+}
+
+translate(p3) {
+  print(p3 > 3 ? translate(p3 >> 1) : '\0');
+  return p3 & 1 ? '-' : '.';
+}
+
+print(p3) char p3;
+{ (void)write(1, &p3, 1); }
