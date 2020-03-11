154e1cc8d78d2eb7f1b3c1349d79e9defd5c7fda
Manuelpv17 <manuelpv17@outlook.com>
2020-03-11 13:23:48 -0500

    Add Task 2. To hell with circumstances; I create opportunities -v2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
  * int_index - searches for an integer.
@@ -9,6 +10,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
