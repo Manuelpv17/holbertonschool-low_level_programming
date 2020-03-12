c3782ce852271a2ce52f5a28297b405565ed68f3
Manuelpv17 <manuelpv17@outlook.com>
2020-03-11 19:14:09 -0500

    Add Task 4. Most hackers are young -v1

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	char *p = (char *)main;
+	int n, i;
+
+	if (argc != 2)
+	{
+		printf("Error");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", p[i]);
+
+		if (i == n - 1)
+		{
+			printf("\n");
+			break;
+		}
+		printf(" ");
+	}
+
+	return (0);
+}
