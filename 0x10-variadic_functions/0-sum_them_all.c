1ac859623978a996eab57b4ae9aeb53d8471afc6
Manuelpv17 <manuelpv17@outlook.com>
2020-03-13 14:20:10 -0500

    Add betty style and comments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,18 +1,21 @@
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - Sum numbers
+ * @n: amount of number to print
+ * Return: sum of numbers
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-        int sum = 0;
-        int i;
-        va_list number;
+	int sum = 0;
+	int i;
+	va_list number;
 
-        va_start(number, n);
+	va_start(number, n);
 
-        for (i = 0; i < n; i++)
-        {
-                sum += va_arg(number, int);
-        }
-        va_end(number);
+	for (i = 0; i < n; i++)
+		sum += va_arg(number, int);
+	va_end(number);
 
-        return(sum);
+	return (sum);
 }
