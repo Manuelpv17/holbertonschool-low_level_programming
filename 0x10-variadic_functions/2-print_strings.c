e95b535b07140ddff444c7bf881e5904ed12d2b8
Manuelpv17 <manuelpv17@outlook.com>
2020-03-14 12:47:42 -0500

    Deleted case where n = 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,9 +12,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s = NULL;
 	va_list strings;
 
-	if (n == 0)
-		return;
-
 	va_start(strings, n);
 
 	for (i = 0; i < n; i++)
