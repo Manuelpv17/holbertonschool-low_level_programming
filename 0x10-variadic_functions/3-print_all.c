46f3678ac410c029eab479c93b8951cc8fc4ba69
Manuelpv17 <manuelpv17@outlook.com>
2020-03-14 15:23:57 -0500

    New code with switch

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,84 +1,48 @@
 #include "variadic_functions.h"
 
-void (*decision(const char))(va_list);
-void charf(va_list);
-void integerf(va_list);
-void floatf(va_list);
-void stringf(va_list);
-void ex(va_list);
-
 /**
  * prints anything - prints anything
- * @format: list of types of arguments passed 
+ * @format: list of types of arguments passed
  */
-
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	va_list unknow_list;
-
-	if (format == NULL)
-		return;
-
-	va_start(unknow_list, format);
-
-	while(format[i] != '\0')
-	{
-		decision(format[i])(unknow_list);	 
-		printf(", ");
-		i++;
-	}
-	printf("\n");
-	va_end(unknow_list);
-}
-
-void (*decision(char d))(va_list)
-{
+	va_list unknown_list;
 	int i = 0;
+	int a = 0;
+	char *s = NULL;
 
-	types_t type1[] = 
+	va_start(unknown_list, format);
+	while (format[i] != '\0' && format != NULL)
 	{
-		{'c', charf}, 
-		{'i', integerf},
-		{'f', floatf},
-		{'s', stringf},
-		{'\0', NULL}
-	};
-
-	while (type1[i].type != '\0')
-	{
-		if (type1[i].type == d)
-		{	
-			return(type1[i].f);
+		a = 0;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(unknown_list, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(unknown_list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(unknown_list, double));
+				break;
+			case 's':
+				s = va_arg(unknown_list, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				a = 1;
+				break;
 		}
 		i++;
+		if (format[i] != '\0' && a != 1)
+			printf(", ");
 	}
-	return (ex);
-}
-
-void ex(va_list char_type)
-{
-}
-
-void charf(va_list char_type)
-{
-	printf("%c", va_arg(char_type, int));
-}
-
-void integerf(va_list char_type)
-{
-	printf("%d", va_arg(char_type, int));
-}
-
-void floatf(va_list char_type)
-{
-	printf("%f", va_arg(char_type, double));
-}
-
-void stringf(va_list char_type)
-{
-	if (char_type == NULL)
-		printf("(nil)");
-	else
-	printf("%s", va_arg(char_type, char *));
+	printf("\n");
+	va_end(unknown_list);
 }
