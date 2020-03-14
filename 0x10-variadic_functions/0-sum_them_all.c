7b44d114e7d354b1956e43d4db2273e23e8f5af7
Manuelpv17 <manuelpv17@outlook.com>
2020-03-14 12:31:17 -0500

    Corrected betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,6 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(number, int);
+
 	va_end(number);
 
 	return (sum);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,21 +4,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-
-typedef struct types
-{
-	char type;
-	void (*f)(va_list);
-} types_t;
-
-
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
