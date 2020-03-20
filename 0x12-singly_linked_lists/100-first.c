6876cff0b9df1580bc092c9a9b9af4bd4101a4ff
Manuelpv17 <manuelpv17@outlook.com>
2020-03-19 21:49:58 -0500

    Add task print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void fast_function(void) __attribute__((constructor));
+
+void fast_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
