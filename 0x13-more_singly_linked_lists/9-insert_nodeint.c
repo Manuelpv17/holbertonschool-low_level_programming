4a658c1fd9a47a6dc9dd338d4eac49aefc05a1d1
Manuelpv17 <manuelpv17@outlook.com>
2020-03-24 11:59:18 -0500

    Add Task 9 insert new node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: Double pointer to head
+ * @idx: index of the list where the new node should be added
+ * @n: Data of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *aux_head = *head;
+	listint_t *p_new_node = NULL;
+
+	p_new_node = malloc(sizeof(listint_t));
+	p_new_node->n = n;
+	p_new_node->next = NULL;
+
+	for (i = 0; i < idx - 1; i++)
+	{
+		aux_head = aux_head->next;
+	}
+
+	p_new_node->next = aux_head->next->next;
+	aux_head->next = p_new_node;
+
+	return (p_new_node);
+}
