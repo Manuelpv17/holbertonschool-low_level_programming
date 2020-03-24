6e22c27e3e3c346e569e3bdc4dbeb839e15271d4
Manuelpv17 <manuelpv17@outlook.com>
2020-03-24 12:04:16 -0500

    Add new line at the end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,6 +5,7 @@
  * @head: Double pointer to head
  * @idx: index of the list where the new node should be added
  * @n: Data of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -13,6 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *p_new_node = NULL;
 
 	p_new_node = malloc(sizeof(listint_t));
+	if (p_new_node == NULL)
+		return (NULL);
+
 	p_new_node->n = n;
 	p_new_node->next = NULL;
 
