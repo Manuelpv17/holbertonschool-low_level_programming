796570770fcf76313a6d3989a59f53b5df0c9d05
Manuelpv17 <manuelpv17@outlook.com>
2020-03-24 12:09:45 -0500

    Add case were head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	p_new_node->n = n;
 	p_new_node->next = NULL;
 
+	if (aux_head == NULL)
+	{
+		*head = p_new_node;
+		return (p_new_node);
+	}
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		aux_head = aux_head->next;
