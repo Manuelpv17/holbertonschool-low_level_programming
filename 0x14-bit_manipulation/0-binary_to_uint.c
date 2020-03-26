557c593a91b5763c71ddd514f34712723fc516c2
Manuelpv17 <manuelpv17@outlook.com>
2020-03-26 12:09:32 -0500

    Correct betty style and add case were b == NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 int _isdigit(int c);
 
 /**
- * int binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: b is pointing to a string of 0 and 1 chars
  * Return: the converted number, or 0 if:
  * there is one or more chars in the string b that is not 0 or 1
@@ -13,6 +13,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0, i, j, k, aux;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (_isdigit(b[i]) != 1)
