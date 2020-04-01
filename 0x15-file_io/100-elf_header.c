59c38222afc09a7f8bd84623cd8e060de97dd751
Manuelpv17 <manuelpv17@outlook.com>
2020-03-31 23:46:34 -0500

    Add Task Elf header v0.1

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <elf.h>
+
+/**
+ * main - Read and print Elf file
+ * @argc: number of arguments - 3 or error
+ * @argv: array with arguments
+ * first name of file to be copy
+ * second name of the new file
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	Elf64_Ehdr the_elf_file;
+	int fdes;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Show me the file! Im not magic");
+		exit(98);
+	}
+
+	fdes = open(argv[1], O_RDONLY);
+	if (fdes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	read(fdes, &the_elf_file, sizeof(Elf32_Ehdr));
+	if (fdes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (the_elf_file.e_ident[0] == 0x7f &&
+	    the_elf_file.e_ident[1] == 'E' &&
+	    the_elf_file.e_ident[2] == 'L' &&
+	    the_elf_file.e_ident[3] == 'F')
+		printf("ELF Header:\n");
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: Not valid ELF\n");
+		exit(98);
+	}
+	close(fdes);
+	return (0);
+}
