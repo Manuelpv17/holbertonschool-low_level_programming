c61717b287c348727115c524163b4272ac45b6af
Manuelpv17 <manuelpv17@outlook.com>
2020-05-04 23:33:02 -0500

    Add Corrections

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/Manuelpv17/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/temp/win.so
+export LD_PRELOAD=/tmp/win.so
