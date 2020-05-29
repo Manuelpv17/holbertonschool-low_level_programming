88db5b08059c976061b2079afa6c2ecb6fec767e
Manuelpv17 <manuelpv17@outlook.com>
2020-05-29 11:52:00 -0500

    Add task 3 hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
+	if (ht == NULL)
+		return (0);
+
 	if (key == NULL || value == NULL)
 		return (0);
 
