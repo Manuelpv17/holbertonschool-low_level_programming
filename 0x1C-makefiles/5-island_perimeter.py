f7e447fe6dad62f5a41eab0d9fa456f1c03d6cfc
Manuelpv17 <manuelpv17@outlook.com>
2020-07-03 20:09:51 -0500

    New limit condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,12 +10,12 @@ def island_perimeter(grid):
     for y in range(len_y):
         for x in range(len_x):
             if grid[y][x] == 1:
-                if grid[y + 1][x] == 0:
+                if y == len_y - 1 or grid[y + 1][x] == 0:
                     perimeter += 1
-                if grid[y - 1][x] == 0:
+                if y == 0 or grid[y - 1][x] == 0:
                     perimeter += 1
-                if grid[y][x + 1] == 0:
+                if x == len_x - 1 or grid[y][x + 1] == 0:
                     perimeter += 1
-                if grid[y][x - 1] == 0:
+                if x == 0 or grid[y][x - 1] == 0:
                     perimeter += 1
     return perimeter
